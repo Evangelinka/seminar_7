c6746354eace592e373ba9ada4b2b474073636f5
«Kristina» <kristinabol1996@gmail.com>
2022-08-30 08:42:09 +0800

    3 задача, замена элементов на нечетных индексах

---
+++ Task_3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса нечётные, и замените эти элементы на их квадраты.
+
+// Например, изначально массив выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Новый массив будет выглядеть вот так:
+// 1 4 7 2
+// 5 81 2 9
+// 8 4 2 4
+
+int[,] CreateMatrix(int rowCount, int columnCount, int min, int max)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = new Random().Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CorrectedMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int column = 0; column < matrix.GetLength(1); column++)
+        {
+            if (row % 2 != 0 && column % 2 != 0)
+            {
+                matrix[row, column] = matrix[row, column] * matrix[row, column];
+            }
+        }
+    }
+}
+
+Console.WriteLine("Исходный массив:");
+int[,] matrix = CreateMatrix(3, 4, 0, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+Console.WriteLine("Новый массив:");
+CorrectedMatrix(matrix);
+PrintMatrix(matrix);
