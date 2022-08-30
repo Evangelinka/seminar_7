aeda475054135b17dd6320f066f761b6a9f29f8e
«Kristina» <kristinabol1996@gmail.com>
2022-08-30 08:41:04 +0800

    2 задача, A = m + n

---
+++ Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateMatrix(int rowCount, int columnCount)
+{
+    int[,] array = new int[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = row + column;
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+
+int length = matrix.Length;
+Console.WriteLine($"\nКоличество элементов в матрице: {length}\n");
+
+PrintMatrix(matrix);
